7a0eae2fe660df067de40d582ec89c794f077728
aoengin <ahmetoguzengin@hotmail.com>
2025-04-10 12:11:25 +0300

    update risc0 version to 2.0.1

--- final-spv/src/lib.rs
+++ final-spv/src/lib.rs
@@ -1,5 +1,6 @@
 use final_circuit::FinalCircuitInput;
 use risc0_zkvm::guest::env;
+use utils::u8_32_to_u32_8;
 use zkvm::ZkvmGuest;
 
 pub mod final_circuit;
@@ -11,28 +12,23 @@ pub mod zkvm;
 pub use risc0_zkvm;
 
 /// The method ID for the header chain circuit.
-const HEADER_CHAIN_GUEST_ID: [u32; 8] = {
+const HEADER_CHAIN_GUEST_ID: [u8; 32] = {
     match option_env!("BITCOIN_NETWORK") {
-        Some(network) if matches!(network.as_bytes(), b"mainnet") => [
-            4046644471, 903675258, 1380784752, 882266853, 3151729575, 835580507, 2476016657,
-            1610521255,
-        ],
-        Some(network) if matches!(network.as_bytes(), b"testnet4") => [
-            2421631365, 3264974484, 821027839, 1335612179, 1295879179, 713845602, 1229060261,
-            258954137,
-        ],
-        Some(network) if matches!(network.as_bytes(), b"signet") => [
-            3853738632, 3158955590, 3791151038, 2143196696, 56017613, 2356936109, 2685546689,
-            1284489822,
-        ],
-        Some(network) if matches!(network.as_bytes(), b"regtest") => [
-            1058249551, 988828560, 601998926, 732987198, 1919237144, 863935989, 2017405856,
-            3942537225,
-        ],
-        None => [
-            4046644471, 903675258, 1380784752, 882266853, 3151729575, 835580507, 2476016657,
-            1610521255,
-        ],
+        Some(network) if matches!(network.as_bytes(), b"mainnet") => {
+            hex_literal::hex!("a764839f5d78b6027146a178338b86c45600828bdc05993c457dd689021b4ebf")
+        }
+        Some(network) if matches!(network.as_bytes(), b"testnet4") => {
+            hex_literal::hex!("3f0e327745831156507c290d7e71ea24829a4c1af0020bac2ed4df9ab1d314f0")
+        }
+        Some(network) if matches!(network.as_bytes(), b"signet") => {
+            hex_literal::hex!("c20d2289319ef9bd0ccb210bda359e3cbd4cab4e08747f9f87b3639c898aac0c")
+        }
+        Some(network) if matches!(network.as_bytes(), b"regtest") => {
+            hex_literal::hex!("bc2b6560aeb587f7dca25c067d72badaadade5ec360b4006bcc76e99c4c6838f")
+        }
+        None => {
+            hex_literal::hex!("a764839f5d78b6027146a178338b86c45600828bdc05993c457dd689021b4ebf")
+        }
         _ => panic!("Invalid network type"),
     }
 };
@@ -41,7 +37,8 @@ const HEADER_CHAIN_GUEST_ID: [u32; 8] = {
 pub fn final_circuit(guest: &impl ZkvmGuest) {
     let start = env::cycle_count();
     let input: FinalCircuitInput = guest.read_from_host::<FinalCircuitInput>();
-    guest.verify(HEADER_CHAIN_GUEST_ID, &input.block_header_circuit_output);
+    let header_chain_guest_id = u8_32_to_u32_8(HEADER_CHAIN_GUEST_ID);
+    guest.verify(header_chain_guest_id, &input.block_header_circuit_output);
     input.spv.verify(
         input
             .block_header_circuit_output
--- header-chain/header-chain-guest/build.rs
+++ header-chain/header-chain-guest/build.rs
@@ -1,50 +1,177 @@
-use std::env;
-use std::process::Command;
+use risc0_build::{embed_methods_with_options, DockerOptionsBuilder, GuestOptionsBuilder};
+use std::{collections::HashMap, env, fs, path::Path};
 
 fn main() {
-    println!("cargo:rerun-if-changed=../build.dockerfile");
+    // Build environment variables
+    println!("cargo:rerun-if-env-changed=SKIP_GUEST_BUILD");
+    println!("cargo:rerun-if-env-changed=REPR_GUEST_BUILD");
+    println!("cargo:rerun-if-env-changed=OUT_DIR");
 
-    if env::var("REPR_GUEST_BUILD").is_ok() {
-        // Get the absolute path to the project root
-        let current_dir = env::current_dir().expect("Failed to get current directory");
-        let project_root = current_dir.parent().unwrap().parent().unwrap();
-        let output_dir = project_root.join("target/riscv-guest/riscv32im-risc0-zkvm-elf/docker");
+    // Compile time constant environment variables
+    println!("cargo:rerun-if-env-changed=BITCOIN_NETWORK");
+    println!("cargo:rerun-if-env-changed=TEST_SKIP_GUEST_BUILD");
 
-        eprintln!("Current directory: {:?}", current_dir);
-        eprintln!("Project root: {:?}", project_root);
-        eprintln!("Output directory: {:?}", output_dir);
+    // Check if we should skip the guest build for tests
+    if let Ok("1" | "true") = env::var("TEST_SKIP_GUEST_BUILD").as_deref() {
+        println!("cargo:warning=Skipping guest build in test. Exiting");
+        return;
+    }
 
-        // Ensure the output directory exists
-        std::fs::create_dir_all(&output_dir).expect("Failed to create output directory");
+    let network = env::var("BITCOIN_NETWORK").unwrap_or_else(|_| {
+        println!("cargo:warning=BITCOIN_NETWORK not set, defaulting to 'mainnet'");
+        "mainnet".to_string()
+    });
+    println!("cargo:warning=Building for Bitcoin network: {}", network);
 
-        let output = Command::new("docker")
-            .args([
-                "buildx",
-                "build",
-                "--platform",
-                "linux/amd64",
-                "-f",
-                "header-chain/build.dockerfile",
-                "--output",
-                &format!("type=local,dest=."),
-                "header-chain", // Use current directory as context
-                "--build-arg",
-                &format!(
-                    "BITCOIN_NETWORK={}",
-                    std::env::var("BITCOIN_NETWORK").unwrap().as_str()
-                ),
-            ])
-            .current_dir(project_root) // Set working directory to project root
-            .output()
-            .expect("Failed to execute Docker command");
-
-        if !output.status.success() {
-            eprintln!("Docker build failed:");
-            eprintln!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-            panic!("Docker build failed");
+    // Check if we should skip the guest build
+    match env::var("SKIP_GUEST_BUILD") {
+        Ok(value) => match value.as_str() {
+            "1" | "true" => {
+                println!("cargo:warning=Skipping guest build");
+                let out_dir = env::var_os("OUT_DIR").unwrap();
+                let out_dir = Path::new(&out_dir);
+                let methods_path = out_dir.join("methods.rs");
+                // Write empty ELF data for mock implementation
+                let elf = r#"
+                pub const HEADER_CHAIN_ELF: &[u8] = &[];
+                pub const HEADER_CHAIN_ID: [u32; 8] = [0u32; 8];
+                "#;
+                return fs::write(methods_path, elf)
+                    .expect("Failed to write mock header chain elf");
+            }
+            "0" | "false" => {
+                println!("cargo:warning=Performing guest build");
+            }
+            _ => {
+                println!("cargo:warning=Invalid value for SKIP_GUEST_BUILD: '{}'. Expected '0', '1', 'true', or 'false'. Defaulting to performing guest build.", value);
+            }
+        },
+        Err(env::VarError::NotPresent) => {
+            println!(
+                "cargo:warning=SKIP_GUEST_BUILD not set. Defaulting to performing guest build."
+            );
+        }
+        Err(env::VarError::NotUnicode(_)) => {
+            println!("cargo:warning=SKIP_GUEST_BUILD contains invalid Unicode. Defaulting to performing guest build.");
         }
     }
 
-    risc0_build::embed_methods();
+    // Use embed_methods_with_options with our custom options
+    let guest_pkg_to_options = get_guest_options(network.clone());
+    embed_methods_with_options(guest_pkg_to_options);
+
+    let is_repr_guest_build = match env::var("REPR_GUEST_BUILD") {
+        Ok(value) => match value.as_str() {
+            "1" | "true" => {
+                println!("cargo:warning=REPR_GUEST_BUILD is set to true");
+                true
+            }
+            "0" | "false" => {
+                println!("cargo:warning=REPR_GUEST_BUILD is set to false");
+                false
+            }
+            _ => {
+                println!("cargo:warning=Invalid value for REPR_GUEST_BUILD: '{}'. Expected '0', '1', 'true', or 'false'. Defaulting to false.", value);
+                false
+            }
+        },
+        Err(env::VarError::NotPresent) => {
+            println!("cargo:warning=REPR_GUEST_BUILD not set. Defaulting to false.");
+            false
+        }
+        Err(env::VarError::NotUnicode(_)) => {
+            println!(
+                "cargo:warning=REPR_GUEST_BUILD contains invalid Unicode. Defaulting to false."
+            );
+            false
+        }
+    };
+
+    if is_repr_guest_build {
+        println!("cargo:warning=REPR_GUEST_BUILD is set to true, copying binary to elfs folder");
+        copy_binary_to_elfs_folder(network);
+    } else {
+        println!(
+            "cargo:warning=REPR_GUEST_BUILD is set to false, not copying binary to elfs folder"
+        );
+    }
+}
+
+fn get_guest_options(network: String) -> HashMap<&'static str, risc0_build::GuestOptions> {
+    let mut guest_pkg_to_options = HashMap::new();
+    // let mut features = Vec::new();
+
+    // // Add Bitcoin network feature if specified
+    // if let Ok(network) = env::var("BITCOIN_NETWORK") {
+    //     println!("cargo:warning=Building for Bitcoin network: {}", network);
+    //     features.push(format!("network-{}", network.to_lowercase()));
+    // }
+
+    let opts = if env::var("REPR_GUEST_BUILD").is_ok() {
+        let current_dir = env::current_dir().expect("Failed to get current dir");
+        let current_dir = current_dir.to_str().expect("Failed to convert path to str");
+        let root_dir = format!("{current_dir}/../..");
+
+        println!(
+            "cargo:warning=Using Docker for guest build with root dir: {}",
+            root_dir
+        );
+
+        let docker_opts = DockerOptionsBuilder::default()
+            .root_dir(root_dir)
+            .env(vec![("BITCOIN_NETWORK".to_string(), network.clone())])
+            .build()
+            .unwrap();
+
+        GuestOptionsBuilder::default()
+            // .features(features)
+            .use_docker(docker_opts)
+            .build()
+            .unwrap()
+    } else {
+        println!("cargo:warning=Guest code is not built in docker");
+        GuestOptionsBuilder::default()
+            // .features(features)
+            .build()
+            .unwrap()
+    };
+
+    guest_pkg_to_options.insert("header-chain-guest", opts);
+    guest_pkg_to_options
+}
+
+fn copy_binary_to_elfs_folder(network: String) {
+    // Get manifest directory
+    let current_dir = env::current_dir().expect("Failed to get current dir");
+    let base_dir = current_dir.join("../..");
+
+    // Create elfs directory if it doesn't exist
+    let elfs_dir = base_dir.join("prover/elfs");
+    if !elfs_dir.exists() {
+        fs::create_dir_all(&elfs_dir).expect("Failed to create elfs directory");
+        println!("cargo:warning=Created elfs directory at {:?}", elfs_dir);
+    }
+
+    // Build source path
+    let src_path = base_dir.join("target/riscv-guest/header-chain-circuit/header-chain-guest/riscv32im-risc0-zkvm-elf/docker/header-chain-guest.bin");
+    if !src_path.exists() {
+        println!(
+            "cargo:warning=Source binary not found at {:?}, skipping copy",
+            src_path
+        );
+        return;
+    }
+
+    // Build destination path with network prefix
+    let dest_filename = format!("{}-header-chain-guest.bin", network.to_lowercase());
+    let dest_path = elfs_dir.join(&dest_filename);
+
+    // Copy the file
+    match fs::copy(&src_path, &dest_path) {
+        Ok(_) => println!(
+            "cargo:warning=Successfully copied binary to {:?}",
+            dest_path
+        ),
+        Err(e) => println!("cargo:warning=Failed to copy binary: {}", e),
+    }
 }
